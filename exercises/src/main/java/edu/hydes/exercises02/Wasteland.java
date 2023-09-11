8e3c019a5ac5a2df63237dde52c441c0fae8ca0d
Sean <hydes@sunypoly.edu>
2023-09-11 12:43:40 -0400

    exercise 02 finished or close to done.

--- exercises/src/main/java/edu/hydes/exercises02/Wasteland.java
+++ exercises/src/main/java/edu/hydes/exercises02/Wasteland.java
@@ -1,8 +1,19 @@
 package edu.hydes.exercises02;
 
-public class Wasteland {
-    public static void main(String [] args) {
-        System.out.println("run run run run");
+public class Wasteland
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Fallout Exercise to demonstrate OOP.");
+
+        NPC bob = new NPC();
+        NPC sue = new NPC();
+        NPC jim = new NPC(h: 90, r: 'E');
+
+        int h = bob.getHealth();
+        h -= 5;
+        bob.setHealth(h);
+
     }
 
 
