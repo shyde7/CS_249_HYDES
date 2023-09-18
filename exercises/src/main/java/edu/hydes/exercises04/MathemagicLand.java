c0aab9c40cace401ac73ff63cddeb1b1eba9c420
Sean <hydes@sunypoly.edu>
2023-09-18 15:30:00 -0400

    Exercise 03 and fixed pathing

---
+++ exercises/src/main/java/edu/hydes/exercises04/MathemagicLand.java
@@ -0,0 +1,8 @@
+package edu.hydes.exercises04;
+
+public class MathemagicLand {
+    public static void main(String[] args){
+
+
+    }
+}
--- exercises/src/main/java/module-info.java
+++ exercises/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module exercisesmod {
     exports edu.hydes.exercises03;
     exports edu.hydes.exercises02;
+    exports edu.hydes.exercises04;
         }
