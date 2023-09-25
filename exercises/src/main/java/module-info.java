9f58aeec76512214192b725afdd83b6db85ec838
Sean <hydes@sunypoly.edu>
2023-09-25 12:05:07 -0400

    Exercise 06 paused

--- exercises/src/main/java/module-info.java
+++ exercises/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module exercisesmod {
     exports edu.hydes.exercises03;
     exports edu.hydes.exercises02;
     exports edu.hydes.exercises04;
+    exports edu.hydes.exercises05;
+    exports edu.hydes.exercises06;
         }
