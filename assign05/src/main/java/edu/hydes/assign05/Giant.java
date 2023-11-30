30d8c7d039eb5599c1c3d29da63b746a9261ec36
Sean <sean.hyde04@gmail.com>
2023-11-30 14:39:17 -0500

    Assign 05 Finished Again

---
+++ assign05/src/main/java/edu/hydes/assign05/Giant.java
@@ -0,0 +1,18 @@
+package edu.hydes.assign05;
+
+public class Giant {
+    private String name;
+
+    public Giant(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String toString(){
+        return name;
+    }
+}
---
+++ exercises/src/test/java/edu/hydes/exercises12/TestRectangle.java
@@ -0,0 +1,31 @@
+package edu.hydes.exercises12;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.annotations.Test;
+
+public class TestRectangle {
+
+    @DataProvider(name = "areaProvider")
+    public Object[][] runAreaProvider(){
+        Object[][] data = {
+                {new Rectangle(5,7), 35.0},
+                {new Rectangle(2,3), 6.0},
+                {new Rectangle(0,8), 0.0}
+        };
+        return data;
+    }
+
+    @Test(groups={"areaChecks"}, dataProvider = "areaProvider")
+    public void test_getArea(Rectangle r, Double area){
+        //Rectangle r = new Rectangle(5,7);
+        Assert.assertEquals(r.getArea(), area);
+
+    }
+
+    @Test()
+    public void test_toString(){
+        Rectangle r = new Rectangle(5,7);
+        Assert.assertEquals(r.toString(), "Incorrect Area");
+    }
+
+}
